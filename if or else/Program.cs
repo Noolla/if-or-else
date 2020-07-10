eb96ba43854edc1321c302fba94262fccb4cb90b
Noola <zaebali.bg@gmail.com>
2020-07-10 20:33:32 +0400

    Add project files.

---
+++ if or else/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if_or_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int x = Convert.ToInt32(text);
+            if (x == 228)
+            {
+                Console.WriteLine("Число равно 228");
+            }
+            else
+            {
+                Console.WriteLine("Число не 228");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
